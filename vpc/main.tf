05895de60caab9dfa849fc1316ffd6dee92e0c59
Fabio Biondi <fbiondi@orcastudios.es>
2025-08-29 15:02:59 +0100

    created terrafrom mechanism to deploy subnets in single o multi azs

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,1 +1,71 @@
+data "aws_availability_zones" "this" {
+  state = "available"
+}
 
+
+locals {
+  # Use 1 AZ if single-AZ, otherwise use first 3 AZs
+  azs = var.multi_az ? slice(data.aws_availability_zones.this.names, 0, 3) : slice(data.aws_availability_zones.this.names, 0, 1)
+
+  # Availability Zones map
+  az_map = { for idx, az in local.azs : az => { index = idx } }
+
+  # Public subnets cidr /24s
+  public_cidr = { for az, o in local.az_map : az => cidrsubnet(var.vpc_cidr, 8, 200 + o.index) }
+
+  # Private subnets cidr /19s
+  private_cidr = { for az, o in local.az_map : az => cidrsubnet(var.vpc_cidr, 3, o.index) }
+
+  common_tags = merge({
+    "Company" = var.Company,
+  }, var.tags)
+}
+
+resource "aws_vpc" "test_vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags                 = merge(local.common_tags, { "Type" = "vpc" }, { "Name" = "${var.Company}-vpc" })
+}
+
+
+resource "aws_internet_gateway" "test_vpc" {
+  vpc_id = aws_vpc.test_vpc.id
+  tags   = merge(local.common_tags, { "Type" = "Internet-GW" }, { "Name" = "${var.Company}-igw" })
+}
+
+
+### Public Subnets ###
+
+resource "aws_subnet" "public-sbn" {
+  for_each                = local.az_map
+  vpc_id                  = aws_vpc.test_vpc.id
+  availability_zone       = each.key
+  cidr_block              = local.public_cidr[each.key]
+  map_public_ip_on_launch = true
+  tags = merge(local.common_tags, {
+    "Type" = "subnet"
+    "AZ"   = each.key
+    "Name" = "${var.Company}-public-${each.value.index}"
+  })
+}
+
+
+resource "aws_route_table" "public-rtb" {
+  vpc_id = aws_vpc.test_vpc.id
+  tags   = merge(local.common_tags, { "Type" = "routetable-public" }, { "Name" = "${var.Company}-public-rtb" })
+}
+
+
+resource "aws_route" "public_route_internet" {
+  route_table_id         = aws_route_table.public-rtb.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.test_vpc.id
+}
+
+
+resource "aws_route_table_association" "public-rtb-assoc" {
+  for_each       = aws_subnet.public-sbn
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public-rtb.id
+}
--- vpc/variables.tf
+++ vpc/variables.tf
@@ -1,1 +1,40 @@
+variable "Company" {
+  description = "The Scope for tagging, example 'QA_POC'."
+  type        = string
+  default     = "QA"
+}
 
+
+variable "vpc_cidr" {
+  description = "the CIDR of the VPC must be a /16"
+  type        = string
+  validation {
+    condition     = cidrnetmask(var.vpc_cidr) == "255.255.0.0"
+    error_message = "Not valid. The VPC CIDR must be a /16 network"
+  }
+}
+
+
+variable "multi_az" {
+  description = "Boolean value: write true if you want a subnet for each Availability Zone; Write false if you want a single Availability Zone with one subnet."
+  type        = bool
+  #  default     = false
+}
+
+
+variable "nat_gateway_provisioning" {
+  description = "if 'single' = 1 NAT Gateway; if 'multi' = 1 NAT per each Availability Zones"
+  type        = string
+  default     = "single"
+  validation {
+    condition     = contains(["single", "multi"], var.nat_gateway_provisioning)
+    error_message = "nat_gateway_provisioning accepted value must be 'single' or 'multi'."
+  }
+}
+
+
+variable "tags" {
+  description = "Additional tags to apply to all resources."
+  type        = map(string)
+  default     = { "Environmenr" = "POC", "Company" = "QA" }
+}
